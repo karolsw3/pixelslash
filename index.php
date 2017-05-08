dcdb92036694d1a13657afa83e37faab8e47f1af
Charles S <Charles S>
2017-05-08 17:01:25 +0200

    Input type=password font change

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <head>
 	<title>Pixel slash alpha</title>
+	<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet"> 
 	<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
