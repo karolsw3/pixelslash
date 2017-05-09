58dd5fde9edb0782b50228b4f77bf646538f3620
piotr-aueternum <piotr.aueternum@gmail.com>
2017-05-09 13:18:20 +0200

    remove unused commented code

--- config.php
+++ config.php
@@ -2,19 +2,6 @@
 	$a = new mysqli("localhost", "root", "root", "pixelslash");
 	$_GLOBALS['a'] = new mysqli("localhost", "root", "root", "pixelslash");
 
-	// /* check connection */
-	// if (mysqli_connect_errno()) {
-	// 	printf("Connect failed: %s\n", mysqli_connect_error());
-	// 	exit();
-	// }
-
-	// /* check if server is alive */
-	// if (mysqli_ping($a)) {
-	// 	printf ("Our connection is ok!\n");
-	// } else {
-	// 	printf ("Error: %s\n", mysqli_error($a));
-	// }
-
 	if (mysqli_connect_errno() != 0){
 		echo '<p>Connecting error: ' . mysqli_connect_error() . '</p>';
 	}
