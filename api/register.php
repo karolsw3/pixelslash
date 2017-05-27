2c2be58ae448d338157d4be7ea8e7c0205e382de
Charles S <Charles S>
2017-05-27 23:33:18 +0200

    login'n'register api

---
+++ api/register.php
@@ -0,0 +1,30 @@
+<?php
+	session_start();
+	include("../config.php");
+	$user = mysqli_real_escape_string($a, $_GET['user']);
+	$password = mysqli_real_escape_string($a, $_GET['password']);
+
+	if($password != $_POST['password_repeat']){
+		echo '{"registered":"error_badPasswords"}';
+		exit();
+	}
+	if(strlen($password) <= '4') {
+        echo '{"registered":"error_tooShortPassword"}';
+		exit();
+    }
+
+	$password = md5($password);
+
+	$query = mysqli_query($a, "select * from users where user='$user'");
+	
+	if(mysqli_num_rows($query) > 0){
+		echo '{"registered":"error_nameAlreadyUsed"}';
+		exit();
+	}else{
+		mysqli_query($a,"insert into users (user,password) values ('$user','$password')");
+		$_SESSION["user_logged"] = true;
+		$_SESSION['login'] = $_POST['user'];
+		echo '{"registered":true}';
+		// foward to logged_user_page.php
+	}
+?>
