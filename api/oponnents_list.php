43d59acb0d5c36ad00abd6e1a9983dc8eb746bbc
Karol Świerczek <karol.sw3@gmail.com>
2017-06-25 20:19:19 +0200

    Oponnents list API fixes

--- api/oponnents_list.php
+++ api/oponnents_list.php
@@ -1,21 +1,20 @@
 <?php
 	session_start();
 	include("../config.php");
+	$min_page = $_GET['page']*10;
+	$max_page = $min_page+10;
 	$login = $_SESSION['login'];
 	$query= mysqli_query($a,"select * from users WHERE user='$login'");
 	$user_info = mysqli_fetch_array($query);
 	$user_lvl = $user_info['lvl'];
-
-	$query = mysqli_query($a, "select * from monsters WHERE lvl>'$user_lvl'-2 AND lvl<'$user_lvl'+2 ORDER BY lvl ASC");
+	$query = mysqli_query($a, "select * from monsters WHERE lvl>'$user_lvl'-2 AND lvl<'$user_lvl'+2 ORDER BY lvl ASC LIMIT $min_page, $max_page");
 	$how_much_monsters = mysqli_num_rows($query);
 
+	$arr = [];
 	for($i=0;$i<$how_much_monsters;$i++){	
 		$monster_info = mysqli_fetch_array($query);
 		$unencoded = array('name' => $monster_info["name"],'lvl' => $monster_info["lvl"], 'hp' => $monster_info["hp"], 'atk' => $monster_info["atk"], 'def' => $monster_info["def"],'image' => $monster_info["image"],'background' => $monster_info["background"],'reward' => $monster_info["reward"]);
-		$arr .= json_encode($unencoded);
-		if($i<$how_much_monsters-1){
-			$arr .= ",";
-		}
+		array_push($arr,$unencoded);
 	}
 	echo json_encode($arr);
 ?>
